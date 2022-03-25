d920d7861b3b6f2255457d561ceb3645c80fff1d
Gabriel Rowan <gabrielrowan82@gmail.com>
2022-03-25 22:24:50 +0000

    added further bootstrap styling

--- index.php
+++ index.php
@@ -13,16 +13,28 @@ $gameHtml = \GabrielApp\Classes\ViewHelpers\GameViewHelper::displayManyGames($ga
 <!doctype html>
 <html lang="en">
 <head>
-    <title>Favourite Games</title>
+    <title>GamesHub</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-secondary">
+<div class="container">
+    <div class="row">
+        <h1 class="my-3">GamesHub</h1>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <span><button class='btn btn-primary' type='submit'><a class='link-light' href='ViewFavourites.php'>View All Favourites</a></button></span>
 
-<h1>GamesHub</h1>
-<button class='btn btn-outline-success btn-lg' type='submit'><a href='ViewFavourites.php'>View All Favourites</a></button>
-<?= $gameHtml?>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <?= $gameHtml?>
+    </div>
+</div>
 
 </body>
 </html>
--- src/Classes/ViewHelpers/GameViewHelper.php
+++ src/Classes/ViewHelpers/GameViewHelper.php
@@ -6,16 +6,14 @@ use GabrielApp\Classes\Entities\GameEntity;
 
 class GameViewHelper {
     public static function displayGame(GameEntity $game) {
-        $result  = "<div class='container'><div class='row'><div class='col text-center'>";
-        $result  = "<div class='card my-5' style='width: 25rem;'>";
-        $result .="<img class='card-img-top' src='" . $game->getThumbnail() . "'/><br>";
+        $result  = "<div class='card my-3 mx-2 text-center bg-light' style='width: 25rem;'>";
+        $result .="<img class='card-img-top mt-3' src='" . $game->getThumbnail() . "'/><br>";
         $result .= "<div class='col text-center'><h1 class='card-title'>" . $game->getTitle() . "</h1>";
         $result .= "<h3 class='card-text my-4 mx-2'>" . $game->getShortDescription() . "</h3>";
         $result .="<form method='post' action='addToFavourites.php'>";
         $result .="<input type='hidden' name='id' value='" . $game->getId() . "'/>";
-        $result .="<button class='btn btn-outline-success btn-lg mb-4' type='submit'> Add To Favourites </button>";
+        $result .="<button class='btn btn-outline-primary btn-lg mb-4' type='submit'> Add To Favourites </button>";
         $result .="</form></div></div>";
-        $result .="</div></div></div>";
         return $result;
     }
 
