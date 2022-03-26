26f53380493956f4a0f8ed38a75dd1ed5c4a2dad
Gabriel Rowan <gabrielrowan82@gmail.com>
2022-03-26 10:28:40 +0000

    added bootstrap styling to ViewFavourites page

--- ViewFavourites.php
+++ ViewFavourites.php
@@ -12,4 +12,34 @@ $gameFavouritesString = \GabrielApp\Classes\Utilities\StringFormatter::formatIds
 $favouritedGames = \GabrielApp\Classes\Hydrators\GameEntityHydrator::getFavouritedGamesFromDB($db, $gameFavouritesString);
 $favouriteGamesHtml = \GabrielApp\Classes\ViewHelpers\GameViewHelper::displayManyGames($favouritedGames);
 
-echo $favouriteGamesHtml;
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <title>GamesHub</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body class="bg-secondary">
+<nav class="navbar navbar-light bg-light border-bottom border-secondary">
+    <div class="container d-flex justify-content-between">
+            <h1>GamesHub</h1>
+            <span><button class='btn btn-primary' type='submit'><a class='text-decoration-none link-light' href='index.php'>Back to Homepage</a></button></span>
+    </div>
+</nav>
+<nav class="navbar navbar-light bg-info border-bottom border-secondary d-flex mb-2">
+    <div class="container d-flex justify-content-center mt-2">
+        <h2 class>Your Favourite Games</h2>
+    </div>
+</nav>
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <?= $favouriteGamesHtml?>
+        </div>
+    </div>
+
+</body>
+</html>
