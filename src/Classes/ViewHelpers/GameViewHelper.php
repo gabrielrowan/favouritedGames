1da5d646d2780b4b440372d84c22926c74d83ff0
Gabriel Rowan <gabrielrowan82@gmail.com>
2022-03-26 09:41:31 +0000

    fixed buttons displaying at different heights

--- src/Classes/ViewHelpers/GameViewHelper.php
+++ src/Classes/ViewHelpers/GameViewHelper.php
@@ -9,11 +9,11 @@ class GameViewHelper {
         $result  = "<div class='card my-3 mx-2 text-center bg-light' style='width: 25rem;'>";
         $result .="<img class='card-img-top mt-3' src='" . $game->getThumbnail() . "'/><br>";
         $result .= "<div class='col text-center'><h1 class='card-title'>" . $game->getTitle() . "</h1>";
-        $result .= "<h3 class='card-text my-4 mx-2'>" . $game->getShortDescription() . "</h3>";
+        $result .= "<h3 class='card-text my-4 mx-2' style='height: 13rem;'>" . $game->getShortDescription() . "</h3></div>";
         $result .="<form method='post' action='addToFavourites.php'>";
         $result .="<input type='hidden' name='id' value='" . $game->getId() . "'/>";
         $result .="<button class='btn btn-outline-primary btn-lg mb-4' type='submit'> Add To Favourites </button>";
-        $result .="</form></div></div>";
+        $result .="</form></div>";
         return $result;
     }
 
